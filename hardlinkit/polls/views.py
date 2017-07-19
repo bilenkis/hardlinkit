35c7314137abb19d0998066296f483440a410cfa
Yury Bilenkis <bil9178@gmail.com>
2017-07-20 00:21:56 +0300

    play with templates and run real voting

--- hardlinkit/polls/views.py
+++ hardlinkit/polls/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choise
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
+    context = { 'latest_question_list': latest_question_list }
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
@@ -16,10 +15,21 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', context)
 
 def results(request, question_id):
-    context = { 'question_id': question_id }
+    question = get_object_or_404(Question, pk=question_id)
+    context = { 'question': question }
     return render(request, 'polls/results.html', context)
 
 def vote(request, question_id):
-    context = { 'vote': vote }
-    return render(request, 'polls/vote.html', context)
-
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choise = question.choise_set.get(pk=request.POST['choise'])
+    except (KeyError, Choise.DoesNotExist):
+        context = {
+            'question': question,
+            'error_message': "You didn't select a choise.",
+        }
+        return render(request, 'polls/detail.html', context)
+    else:
+        selected_choise.votes += 1
+        selected_choise.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
