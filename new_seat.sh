c49bd85db30091df6e2a9117fdd2fc463949b265
Yuriy Bilenkis <yura@ostrovok.ru>
2017-08-10 23:35:35 +0300

    add init script

--- new_seat.sh
+++ new_seat.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 project=$(basename "$PWD")
 mkdir ../venv
+mkdir -p ~/opt/docker/"$project"
 virtualenv -p python3 ../venv/"$project"
 source ../venv/"$project"/bin/activate
 pip install -r requirements.txt
+docker-compose up -d
+db_name="$project"
+docker exec $project-db psql -U postgres -c "CREATE DATABASE $db_name;"
+cd "$project"
+python manage.py migrate
+python manage.py createsuperuser
